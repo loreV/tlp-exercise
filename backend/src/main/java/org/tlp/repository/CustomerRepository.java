9db1993febae3f8aee9703e15ad984b6d25aa66b
Lorenzo Verri <lore.verri@gmail.com>
2023-02-21 00:02:54 +0100

    Added repositories

--- backend/src/main/java/org/tlp/repository/CustomerRepository.java
+++ backend/src/main/java/org/tlp/repository/CustomerRepository.java
@@ -1,10 +1,11 @@
 package org.tlp.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.tlp.entity.CustomerEntity;
 
 import java.util.List;
-
+@Repository
 public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
     List<CustomerEntity> findAll();
 }
--- backend/src/main/java/org/tlp/repository/DeviceRepository.java
+++ backend/src/main/java/org/tlp/repository/DeviceRepository.java
@@ -1,7 +1,22 @@
 package org.tlp.repository;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.tlp.entity.DeviceEntity;
 
+import java.util.List;
+@Repository
 public interface DeviceRepository extends CrudRepository<DeviceEntity, Long> {
+    List<DeviceEntity> findAll();
+
+    @Modifying
+    @Transactional
+    @Query("delete from Device b where b.uuid=:uuid")
+    void deleteDevice(@Param("uuid") String uuid);
+
+    DeviceEntity findByUuid(String uuid);
 }
